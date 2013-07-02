a108720698eaa4c2ea95c4b068c40e9f7b2bc464
Luis Cordova <cordoval@gmail.com>
2013-07-01 19:44:44 -0500

    first solution

--- src/Dragon/PathEngine.php
+++ src/Dragon/PathEngine.php
@@ -6,16 +6,54 @@ class PathEngine
 {
     const RIGHT = 1;
     const LEFT = -1;
+    const length = 120;
+    const nodes = 5;
+
+    public $d;
+
+    public function f($x, $y)
+    {
+        if ($y < 0) {
+            return 0;
+        }
+
+        if ($y == 0){
+            if ($x == 0) {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        $r = $this->d[$x][$y];
+        if ($r != -1) {
+            return $r;
+        }
+
+        $r = 0;
+        $r = $r + $this->f(($x + 1) % self::nodes, $y - 1);
+        $r = $r + $this->f(($x - 1 + self::nodes) % self::nodes, $y - 1);
+
+        return $r;
+    }
 
     public function run()
     {
-        $totalPaths = gmp_fact(120);
+        $this->initDMatrix();
 
-        $hasSamefinalOrientation = function($path) {
-            return array_sum($path) % 5 == 0;
-        };
+        $this->f(0, self::length);
 
-        return $totalPaths;
-        //return sizeof($paths);
+        //$totalPaths = gmp_fact(self::n);
+        //$hasSamefinalOrientation = function($path) {
+        //    return array_sum($path) % 5 == 0;
+        //};
+    }
+
+    public function initDMatrix()
+    {
+        foreach(range(0,100) as $i) {
+            $this->d[$i] = array_fill(1, 120, -1);
+        }
     }
 }
+
